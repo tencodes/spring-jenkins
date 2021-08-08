220031cc372b19708a6af69c5c4479bd22cd729a
Tenzin <tenzin@localhost.localdomain>
2021-08-08 13:27:56 -0700

    added third logger statement

--- src/test/java/com/tenzin/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/tenzin/jenkins/api/SpringJenkinsApplicationTests.java
@@ -19,6 +19,7 @@ class SpringJenkinsApplicationTests {
 	public void contextLoads() {
 		logger.info("Test case executing");
 		logger.info("Test case executing second log statement.....");
+		logger.info("Test case executing third log statement......");
 		assertEquals(true, true);
 	}
 
